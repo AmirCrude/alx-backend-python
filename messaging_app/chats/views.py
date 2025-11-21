c9394f67450d9b63c0e9864ff2acbd768fd7bf2f
Amir Abdu <amirabdu.com@gmail.com>
2025-11-21 09:23:43 +0300

    Add URL routing with DefaultRouter and fix NameError in views

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, permissions, filters
+from rest_framework import viewsets, permissions, filters, serializers
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from .models import Conversation, Message, User
