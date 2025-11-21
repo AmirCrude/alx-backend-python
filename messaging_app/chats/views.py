75535f95bf9e360275911e01c165469045b3ea9a
Amir Abdu <amirabdu.com@gmail.com>
2025-11-21 09:08:02 +0300

    Add URL routing for Conversation and Message ViewSets in messaging app

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, permissions, serializers
+from rest_framework import viewsets, permissions, filters
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from .models import Conversation, Message, User
